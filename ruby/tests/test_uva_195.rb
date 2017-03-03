e29d523bd3945d8a9b31bcf47a120e5ad6a9f9b8
quatroka <monteiro.leonardosantos@gmail.com>
2017-03-03 04:34:08 -0300

    finish the basic one

---
+++ ruby/tests/test_uva_195.rb
@@ -0,0 +1,25 @@
+# Test File for UVa problem 195 - Anagram
+
+require_relative "../uva_195_with_modules"
+require "test/unit"
+
+
+# Test class for testing permutations.
+class TestPermutation < Test::Unit::TestCase
+  # Test if method get_amount_results returns the correct
+  # permutation amount.
+  def test_amount_results
+    assert_equal(6, get_permutation_list('abc').length)
+    assert_equal(24, get_permutation_list('abcd').length)
+    assert_equal(1, get_permutation_list('aaa').length)
+    assert_equal(3, get_permutation_list('aab').length)
+  end
+
+  # Test if method get_permutation_list returns the 
+  # permutation list corretily.
+  def test_permutation_list
+    assert_equal(["aaa"], get_permutation_list('aaa'))
+    assert_equal(["abc", "acb", "bac", "bca", "cab", "cba"],
+                  get_permutation_list('abc'))
+  end
+end
---
+++ ruby/uva_195_with_modules.rb
@@ -0,0 +1,30 @@
+# File to resolve UVa problem 195 - Anagra.
+# https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=131
+
+
+# Receive a letters sequence return your permutation.
+#
+# * Receive a letters sequence in string format, split that
+# * in a list of characters and create a list of permutation
+# * these lisf of characters.
+def get_permutation_list(letters_sequence)
+  letters_sequence = letters_sequence.split(//)
+
+  permutation_list = letters_sequence.permutation.to_a
+
+  permutation_list = permutation_list.map{|x| x.join('')}
+
+  return permutation_list.uniq
+end
+
+n_cases = gets.to_i
+
+result_list = []
+for i in 1..n_cases do
+  permutation_list = get_permutation_list(gets.chomp)
+  result_list << permutation_list.sort
+end
+
+for j in 0..(result_list.length - 1) do
+    puts result_list[j]
+end
